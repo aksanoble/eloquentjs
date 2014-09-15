a15dad9023ee0bc4b7d19309f5be14931b480955
aksanoble <aksanoble@gmail.com>
2014-09-15 17:16:27 +0530

    Using for loop

--- ejs02.js
+++ ejs02.js
@@ -1,6 +1,16 @@
+//Using string length as mentioned in the hint
+
 var string = "#";
 var hash = "#";
 while(string.length <=7) {
     console.log(string);
     string =string+ hash;
+}
+
+//Using For Loop
+var string = "#";
+var hash = "#";
+for (var i = 1; i<=7; i++) {
+    console.log(string);
+    string = string + hash;
 }
