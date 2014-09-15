b1a6a8aade8b6147b661a3800d7ef9242758bcae
aksanoble <aksanoble@gmail.com>
2014-09-15 17:31:04 +0530

    FizzBuzz

--- ejs02.js
+++ ejs02.js
@@ -14,3 +14,23 @@ for (var i = 1; i<=7; i++) {
     console.log(string);
     string = string + hash;
 }
+
+//Fizz and Buzz seperate
+for (var i = 1; i<=100; i++) {
+    if (i%3 == 0 && i%5 != 0)
+        console.log("Fizz");
+    else if (i%3 != 0 && i%5 ==0)
+        console.log("Buzz");
+    else console.log(i);
+}
+
+//Now FizzBuzz for divisible by both 3 and 5
+for (var i = 1; i<=100; i++) {
+    if (i%3 == 0 && i%5 != 0)
+        console.log("Fizz");
+    else if (i%3 != 0 && i%5 ==0)
+        console.log("Buzz");
+    else if (i%3 == 0 && i%5 ==0)
+        console.log("FizzBuzz");
+    else console.log(i);
+}
