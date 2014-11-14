41f24923c59b415e14711a3578e7ce758f6f5555
aksanoble <aksanoble@gmail.com>
2014-11-14 19:00:20 +0530

    counting beans

--- ejs03/ejs03_02.js
+++ ejs03/ejs03_02.js
@@ -1,3 +1,4 @@
+//Recursion
 function isEven(number) {
   if (number == 1)
     return false;
@@ -11,3 +12,28 @@ console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
 console.log(isEven(-5));
+
+//Counting Beans
+var B = 0;
+function countBs(s) {
+  stringLength = s.length;
+  for (i = 0; i < stringLength; i++) {
+	 if (s.charAt(i) == "B")
+       B = B+1;
+   }
+  return B ;
+}
+console.log(countBs("bbbC"));
+
+//Counting any letter
+function countChar(s,y) {
+  var count = 0;
+  stringLength = s.length;
+  for (i = 0; i < stringLength; i++) {
+	 if (s.charAt(i) == y)
+       count = count+1;
+   }
+  return count ;
+}
+console.log(countChar("bbbC", "b"));
+console.log(countChar("kakkerlak", "k"));
