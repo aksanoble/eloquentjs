ae55a498819b7ba8492401f8651a88cdc9431f96
aksanoble <aksanoble@gmail.com>
2014-11-14 17:41:45 +0530

    removing files moved to the folder

--- ejs02_01.js
+++
@@ -1,8 +0,0 @@
-//Using string length as mentioned in the hint
-
-var string = "#";
-var hash = "#";
-while(string.length <=7) {
-    console.log(string);
-    string =string+ hash;
-}
--- ejs02_02.js
+++
@@ -1,8 +0,0 @@
-//Fizz and Buzz seperate
-for (var i = 1; i<=100; i++) {
-    if (i%3 == 0 && i%5 != 0)
-        console.log("Fizz");
-    else if (i%3 != 0 && i%5 ==0)
-        console.log("Buzz");
-    else console.log(i);
-}
