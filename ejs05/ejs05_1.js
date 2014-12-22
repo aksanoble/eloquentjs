4d76947366af07a4961da724a5b02485a03f87cc
aksanoble <aksanoble@gmail.com>
2014-12-22 16:13:08 +0530

    including reduce

--- ejs05/ejs05_1.js
+++ ejs05/ejs05_1.js
@@ -5,3 +5,8 @@ for (var i=0; i< arrays.length; i++) {
 flatArray = flatArray.concat(arrays[i]);
 };
 console.log(flatArray);
+
+//After looking at the solution
+var arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.reduce(function (start,current) {
+  return start.concat(current);}));
